16ee0722527db1c6d3eec455d36d4f0ed35e9732
dgmyers08 <39773449+dgmyers08@users.noreply.github.com>
2018-06-24 22:11:39 -0600

    Yii2/Wuala App

---
+++ 1/config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=wualals.cfqtgrhpbhtg.us-east-2.rds.amazonaws.com;dbname=wualals',
+    'username' => 'wualaAdmin',
+    'password' => 'keepWorking++',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
