36587e70f24b1a5eba56a6244bf71fe6e89d86e5
dgmyers08 <39773449+dgmyers08@users.noreply.github.com>
2018-09-28 00:19:36 -0600

    minor

--- 3/config/db.php
+++ 3/config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=mysql.wualaapp.com;dbname=wuala',
     'username' => 'wualaadmin',
-    'password' => 'jollyS!gn36',
+    'password' => 'lumpyGoos360',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
