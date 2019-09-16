0d54c974f78289059de5d357939f99ec4a36cea0
dgmyers08 <39773449+dgmyers08@users.noreply.github.com>
2019-09-15 22:43:54 -0600

    q1_2 and firebase

---
+++ dev/wualaapp/q1_2/src/pages/Index.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-page class="flex flex-center">
+    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+    <q-btn @click="firebaseTestWrite()" color="white" text-color="black" label="Firebase Write Test"/>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      userId: 1,
+      username: 'Geoff',
+      email: 'dgmyers08@gmail.com'
+    }
+  },
+  methods: {
+    firebaseTestWrite () {
+      console.log('firebaseTestWrite')
+      this.$firestore.collection('users').doc('123abc').set({
+        userId: this.userId,
+        username: this.username,
+        email: this.email
+      }).then(response => {
+        console.log(response)
+      }).catch(error => {
+        console.log(error)
+      })
+    }
+  }
+}
+</script>
